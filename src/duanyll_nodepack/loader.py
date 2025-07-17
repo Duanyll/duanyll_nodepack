7e68919a45dbf9f55dfa16972d8d86b9bedeb090
duanyll <duanyll@outlook.com>
2025-07-17 04:12:32 +0000

    add hf loaders

---
+++ src/duanyll_nodepack/loader.py
@@ -0,0 +1,338 @@
+import os.path
+import json
+import torch
+import huggingface_hub
+
+import comfy.utils
+import comfy.sd
+import folder_paths
+
+
+def get_checkpoint_file_from_hf(
+    repo_id: str, subfolder: str = None, filename: str = None
+) -> str:
+    if subfolder is None:
+        subfolder = ""
+    if filename is not None:
+        return os.path.join(subfolder, filename)
+
+    # List files in the subfolder of the Hugging Face repository
+    files = [
+        f
+        for f in huggingface_hub.list_repo_tree(repo_id, path_in_repo=subfolder)
+        if isinstance(f, huggingface_hub.hf_api.FileInfo)
+    ]
+
+    suffixes = [".index.json", ".safetensors", ".sft", ".bin", ".pt"]
+
+    for suffix in suffixes:
+        matching_files = [f.rfilename for f in files if f.rfilename.endswith(suffix)]
+        if len(matching_files) > 1:
+            raise ValueError(
+                f"Multiple files found with suffix '{suffix}' in subfolder '{subfolder}' of repository '{repo_id}'. Please specify a filename."
+            )
+        elif len(matching_files) == 1:
+            return matching_files[0]
+
+    raise ValueError(
+        f"No files found with the expected suffixes in subfolder '{subfolder}' of repository. Please specify a filename."
+    )
+
+
+def load_checkpoint_from_hf(
+    repo_id: str, subfolder: str = None, filename: str = None, return_metadata=False
+):
+    checkpoint_file = get_checkpoint_file_from_hf(repo_id, subfolder, filename)
+
+    if checkpoint_file.endswith(".index.json"):
+        # Load the index file to get the actual checkpoint file
+        index_path = huggingface_hub.hf_hub_download(
+            repo_id=repo_id, filename=checkpoint_file
+        )
+        with open(index_path, "r") as f:
+            index_data = json.load(f)
+        # Actual checkpoint files are unique values in 'weight_map'
+        checkpoint_files = set(index_data["weight_map"].values())
+        state_dicts = []
+        for file in checkpoint_files:
+            file_path = huggingface_hub.hf_hub_download(repo_id=repo_id, filename=file)
+            state_dicts.append(comfy.utils.load_state_dict(file_path))
+        # Merge all state dicts
+        merged_state_dict = {}
+        for state_dict in state_dicts:
+            for key, value in state_dict.items():
+                if key in merged_state_dict:
+                    merged_state_dict[key] += value
+                else:
+                    merged_state_dict[key] = value
+        if return_metadata:
+            return merged_state_dict, None
+        else:
+            return merged_state_dict
+    else:
+        # Directly load the checkpoint file
+        file_path = huggingface_hub.hf_hub_download(
+            repo_id=repo_id, filename=checkpoint_file
+        )
+        state_dict, metadata = comfy.utils.load_state_dict(
+            file_path, return_metadata=True
+        )
+        if return_metadata:
+            return state_dict, metadata
+        else:
+            return state_dict
+
+
+class HfCheckpointLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "repo_id": ("STRING", {"default": ""}),
+            },
+            "optional": {
+                "subfolder": ("STRING", {"default": ""}),
+                "filename": ("STRING", {"default": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE")
+    FUNCTION = "load_checkpoint"
+    CATEGORY = "duanyll"
+
+    def load_checkpoint(self, repo_id, subfolder, filename):
+        if not repo_id:
+            raise ValueError("Repository ID cannot be empty.")
+
+        if not subfolder:
+            subfolder = None
+
+        if not filename:
+            filename = None
+
+        state_dict, metadata = load_checkpoint_from_hf(
+            repo_id, subfolder, filename, return_metadata=True
+        )
+
+        out = comfy.sd.load_checkpoint_guess_config(
+            state_dict,
+            output_model=True,
+            output_vae=True,
+            output_clip=True,
+            embedding_directory=folder_paths.get_folder_paths("embeddings"),
+            metadata=metadata,
+        )
+
+        return out[:3]
+
+
+class HfVaeLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "repo_id": ("STRING", {"default": ""}),
+            },
+            "optional": {
+                "subfolder": ("STRING", {"default": ""}),
+                "filename": ("STRING", {"default": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("VAE",)
+    FUNCTION = "load_vae"
+    CATEGORY = "duanyll"
+
+    def load_vae(self, repo_id, subfolder, filename):
+        if not repo_id:
+            raise ValueError("Repository ID cannot be empty.")
+
+        if not subfolder:
+            subfolder = None
+
+        if not filename:
+            filename = None
+
+        state_dict, metadata = load_checkpoint_from_hf(
+            repo_id, subfolder, filename, return_metadata=True
+        )
+
+        vae = comfy.sd.VAE(sd=state_dict)
+        vae.throw_exception_if_invalid()
+
+        return (vae,)
+
+
+class HfLoraLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": (
+                    "MODEL",
+                    {"tooltip": "The diffusion model the LoRA will be applied to."},
+                ),
+                "clip": (
+                    "CLIP",
+                    {"tooltip": "The CLIP model the LoRA will be applied to."},
+                ),
+                "repo_id": ("STRING", {"default": ""}),
+                "strength_model": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": -100.0,
+                        "max": 100.0,
+                        "step": 0.01,
+                        "tooltip": "How strongly to modify the diffusion model. This value can be negative.",
+                    },
+                ),
+                "strength_clip": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": -100.0,
+                        "max": 100.0,
+                        "step": 0.01,
+                        "tooltip": "How strongly to modify the CLIP model. This value can be negative.",
+                    },
+                ),
+            },
+            "optional": {
+                "subfolder": ("STRING", {"default": ""}),
+                "filename": ("STRING", {"default": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("MODEL", "CLIP")
+    FUNCTION = "load_lora"
+    CATEGORY = "duanyll"
+
+    def load_lora(
+        self, model, clip, repo_id, strength_model, strength_clip, subfolder, filename
+    ):
+        if not repo_id:
+            raise ValueError("Repository ID cannot be empty.")
+
+        if not subfolder:
+            subfolder = None
+
+        if not filename:
+            filename = None
+
+        state_dict, metadata = load_checkpoint_from_hf(
+            repo_id, subfolder, filename, return_metadata=True
+        )
+
+        model_lora, clip_lora = comfy.sd.load_lora_for_models(
+            model, clip, state_dict, strength_model, strength_clip
+        )
+        return (model_lora, clip_lora)
+
+
+class HfLoraLoaderModelOnly:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": (
+                    "MODEL",
+                    {"tooltip": "The diffusion model the LoRA will be applied to."},
+                ),
+                "repo_id": ("STRING", {"default": ""}),
+                "strength_model": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": -100.0,
+                        "max": 100.0,
+                        "step": 0.01,
+                        "tooltip": "How strongly to modify the diffusion model. This value can be negative.",
+                    },
+                ),
+            },
+            "optional": {
+                "subfolder": ("STRING", {"default": ""}),
+                "filename": ("STRING", {"default": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "load_lora"
+    CATEGORY = "duanyll"
+
+    def load_lora(self, model, repo_id, strength_model, subfolder, filename):
+        if not repo_id:
+            raise ValueError("Repository ID cannot be empty.")
+
+        if not subfolder:
+            subfolder = None
+
+        if not filename:
+            filename = None
+
+        state_dict, metadata = load_checkpoint_from_hf(
+            repo_id, subfolder, filename, return_metadata=True
+        )
+
+        model_lora = comfy.sd.load_lora_for_models(
+            model, None, state_dict, strength_model, 0.0
+        )[0]
+        return (model_lora,)
+
+
+class HfDualClipLoader:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "repo_id_1": ("STRING", {"default": ""}),
+                "repo_id_2": ("STRING", {"default": ""}),
+                "type": (["sdxl", "sd3", "flux", "hunyuan_video", "hidream"],),
+            },
+            "optional": {
+                "subfolder_1": ("STRING", {"default": ""}),
+                "subfolder_2": ("STRING", {"default": ""}),
+                "filename_1": ("STRING", {"default": ""}),
+                "filename_2": ("STRING", {"default": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("CLIP",)
+    FUNCTION = "load_dual_clip"
+    CATEGORY = "duanyll"
+
+    def load_clip(self, repo_id_1, repo_id_2, type, subfolder_1, subfolder_2, filename_1, filename_2):
+        clip_type = getattr(
+            comfy.sd.CLIPType, type.upper(), comfy.sd.CLIPType.STABLE_DIFFUSION
+        )
+
+        if not repo_id_1 or not repo_id_2:
+            raise ValueError("Both repository IDs cannot be empty.")
+        
+        if not subfolder_1:
+            subfolder_1 = None
+        
+        if not subfolder_2:
+            subfolder_2 = None
+            
+        if not filename_1:
+            filename_1 = None
+            
+        if not filename_2:
+            filename_2 = None
+            
+        state_dict_1, metadata_1 = load_checkpoint_from_hf(
+            repo_id_1, subfolder_1, filename_1, return_metadata=True
+        )
+        state_dict_2, metadata_2 = load_checkpoint_from_hf(
+            repo_id_2, subfolder_2, filename_2, return_metadata=True
+        )
+        
+        clip = comfy.sd.load_text_encoder_state_dicts(
+            state_dicts=[state_dict_1, state_dict_2],
+            embedding_directory=folder_paths.get_folder_paths("embeddings"),
+            clip_type=clip_type,
+        )
+        
+        return (clip,)
--- src/duanyll_nodepack/nodes.py
+++ src/duanyll_nodepack/nodes.py
@@ -1,6 +1,13 @@
 from .photododdle import PhotoDoddleConditioning
 from .difference import ImageDifferenceCmap
 from .kontext import FluxKontextTrue3DPE
+from .loader import (
+    HfCheckpointLoader,
+    HfDualClipLoader,
+    HfLoraLoader,
+    HfLoraLoaderModelOnly,
+    HfVaeLoader,
+)
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
@@ -8,6 +15,11 @@ NODE_CLASS_MAPPINGS = {
     "PhotoDoddleConditioning": PhotoDoddleConditioning,
     "ImageDifferenceCmap": ImageDifferenceCmap,
     "FluxKontextTrue3DPE": FluxKontextTrue3DPE,
+    "HfCheckpointLoader": HfCheckpointLoader,
+    "HfDualClipLoader": HfDualClipLoader,
+    "HfLoraLoader": HfLoraLoader,
+    "HfLoraLoaderModelOnly": HfLoraLoaderModelOnly,
+    "HfVaeLoader": HfVaeLoader,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -15,4 +27,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "PhotoDoddleConditioning": "PhotoDoddle Conditioning",
     "ImageDifferenceCmap": "Image Difference with Colormap",
     "FluxKontextTrue3DPE": "Patch Flux Kontext True 3D PE",
+    "HfCheckpointLoader": "HuggingFace Checkpoint Loader",
+    "HfDualClipLoader": "HuggingFace Dual CLIP Loader",
+    "HfLoraLoader": "HuggingFace LoRA Loader",
+    "HfLoraLoaderModelOnly": "HuggingFace LoRA Loader (Model Only)",
+    "HfVaeLoader": "HuggingFace VAE Loader",
 }
